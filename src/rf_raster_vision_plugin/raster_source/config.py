2b58f1cfef93e9b529d5a6ccba8da0b30d4a0215
James Santucci <james.santucci@gmail.com>
2019-06-18 09:53:07 -0400

    Use comment type annotations instead, add config property

--- src/rf_raster_vision_plugin/raster_source/config.py
+++ src/rf_raster_vision_plugin/raster_source/config.py
@@ -17,13 +17,13 @@ class RfRasterSourceConfig(RasterSourceConfig):
 
     def __init__(
         self,
-        project_id: Optional[UUID],
-        project_layer_id: Optional[UUID],
-        refresh_token: Optional[str],
-        channel_order: List[int],
-        num_channels: Optional[int],
-        rf_api_host: Optional[str],
-    ) -> None:
+        project_id,  # type: Optional[UUID]
+        project_layer_id,  # type: Optional[UUID]
+        refresh_token,  # type: Optional[str]
+        channel_order,  # type: List[int]
+        num_channels,  # type: Optional[int]
+        rf_api_host,  # type: Optional[str]
+    ):
         self.project_id = project_id or self.project_id
         self.project_layer_id = project_layer_id or self.project_layer_id
         self.refresh_token = refresh_token or self.refresh_token
@@ -31,16 +31,45 @@ class RfRasterSourceConfig(RasterSourceConfig):
         self.num_channels = num_channels or self.num_channels
         self.rf_api_host = rf_api_host or self.rf_api_host
 
+    def create_local(self, tmp_dir):
+        pass
+
+    def create_source(self, tmp_dir, crs_transformer, extent, class_map):
+        pass
+
+    def for_prediction(self, image_uri):
+        pass
+
 
 class RfRasterSourceConfigBuilder(ConfigBuilder):
 
+    _properties = [
+        "project_id",
+        "project_layer_id",
+        "refresh_token",
+        "channel_order",
+        "num_channels",
+        "rf_api_host",
+    ]
     config_class = RfRasterSourceConfig
     T = TypeVar("T")
+
+    def __init__(self):
+        super(ConfigBuilder, self).__init__()
+        self.rf_api_host = "app.staging.rasterfoundry.com"
 
     def with_property(self, property_name: str, property_value: T):
         b = deepcopy(self)
         setattr(b, property_name, property_value)
         return b
+
+    @property
+    def config(self):
+        return {
+            k: getattr(self, k)
+            for k in self._properties
+            if getattr(self, k) is not None
+        }
 
     def from_proto(self, msg):
         return (
