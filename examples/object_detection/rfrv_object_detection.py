1855e6dcb42e633b7b3ee7ce9b40b361401b529e
James Santucci <james.santucci@gmail.com>
2019-06-26 18:14:13 -0400

    Bail on custom plugin

--- examples/object_detection/rfrv_object_detection.py
+++ examples/object_detection/rfrv_object_detection.py
@@ -1,35 +1,84 @@
-from examples.utils import str_to_bool
-from rf_raster_vision_plugin.evaluate.config import VisionObjectDetectionEvaluatorConfigBuilder
-from rf_raster_vision_plugin.raster_source.config import RfRasterSourceConfigBuilder
-from rf_raster_vision_plugin.label_source.config import RfLabelSourceConfigBuilder
-from rf_raster_vision_plugin.label_store.config import RfLabelStoreConfigBuilder
-import rf_raster_vision_plugin.http.raster_foundry as rf
-from rf_raster_vision_plugin.http import vision
-import rastervision as rv
-from rastervision.task.task_config import TaskConfig
-from rastervision.data.dataset_config import DatasetConfig
-from rastervision.data.raster_source.rasterio_source_config import RasterioSourceConfig
-from rastervision.data.scene_config import SceneConfig
-from rastervision.data.raster_transformer.stats_transformer_config import StatsTransformerConfig
-from rastervision.experiment.experiment_config import ExperimentConfig
-from rastervision.rv_config import RVConfig
-import numpy as np
+import json
 import os
+from examples.utils import str_to_bool
+import rf_raster_vision_plugin.http.raster_foundry as rf
+import rastervision as rv
+from rastervision.data.raster_source.rasterio_source_config import RasterioSourceConfig
+from rastervision.data.label_source.object_detection_label_source_config import ObjectDetectionLabelSourceConfig
+from rastervision.data.label_store.object_detection_geojson_store_config import ObjectDetectionGeoJSONStoreConfig
+from rastervision.evaluation.object_detection_evaluator_config import ObjectDetectionEvaluatorConfig
+from rastervision.rv_config import RVConfig
+from everett.manager import NamespacedConfig
+import numpy as np
 from urllib.parse import unquote
-
 
 words = [x.strip() for x in open('/opt/plugin/american-english', 'r').readlines()
          if "'" not in x]
-short_hash = '-'.join(np.random.choice(words, 2))
+random_name = '-'.join(np.random.choice(words, 2))
 print('***************************')
-print('Tmp directory is: ', short_hash)
+print('Experiment name is: ', random_name)
 print('***************************')
-tmp_dir = '/tmp/' + short_hash
-os.mkdir(tmp_dir)
+
+
+def _to_rv_feature(annotation: dict, class_map: dict) -> dict:
+    """Convert the geojson from Raster Foundry's API into the minimum dict required by Raster Vision
+    """
+
+    return {
+        "geometry": annotation["geometry"],
+        "properties": {"class_id": class_map[annotation["properties"]["label"]]},
+    }
+
+
+def setup(tmp_dir: str, rf_config: NamespacedConfig, name: str) -> dict:
+    rf_project_id = rf_config('project_id')
+    rf_project_layer_id = rf_config('project_layer_id')
+    refresh_token = rf_config('refresh_token')
+    rf_host = rf_config('rf_api_host')
+    ground_truth_annotation_group = rf_config('ground_truth_annotation_group')
+
+    token = 'Bearer ' + rf.get_api_token(refresh_token, rf_host)
+    rf_scenes = rf.get_rf_scenes(token, rf_host, rf_project_id, rf_project_layer_id)
+    project = rf.get_project(token, rf_host, rf_project_id)
+    colors = ["red", "green", "blue", "yellow"]
+    class_map = {
+        item["id"]: idx + 1 for idx, item in enumerate(project["extras"]["annotate"]["labels"])
+    }
+    labels = rf.get_labels(
+        token,
+        rf_host,
+        rf_project_id,
+        rf_project_layer_id,
+        ground_truth_annotation_group,
+        None
+    )
+
+    geojson = {
+        "features": [
+            _to_rv_feature(feat, class_map) for feat in labels["features"]
+        ]
+    }
+    label_path = os.path.join(tmp_dir, 'rf-labels.geojson')
+    with open(label_path, 'w') as outf:
+        outf.write(json.dumps(geojson))
+
+    rs_config_builder = RasterioSourceConfig.builder(rv.RASTERIO_SOURCE).with_uris(
+        [unquote(x["ingestLocation"]) for x in rf_scenes]
+    ).with_channel_order([0, 1, 2])
+    label_source_config_builder = ObjectDetectionLabelSourceConfig.builder(rv.OBJECT_DETECTION) \
+        .with_uri(label_path)
+
+    return {
+        "raster_source_builder": rs_config_builder,
+        "label_source_builder": label_source_config_builder,
+        "task_class_map": {
+            k: (v, colors[v - 1]) for k, v in class_map.items()
+        }
+    }
 
 
 class ObjectDetectionExperiments(rv.ExperimentSet):
-    def exp_rfrv(self, root_uri=tmp_dir, test=True):
+    def exp_rfrv(self, root_uri="/tmp", test=True):
         """Object detection experiment on xView data.
 
         Run the data prep notebook before running this experiment. Note all URIs can be
@@ -46,7 +95,6 @@ class ObjectDetectionExperiments(rv.ExperimentSet):
         batch_size = 16
         debug = False
 
-        RVConfig.set_tmp_dir(tmp_dir)
         config_instance = RVConfig.get_instance()
         rf_config = config_instance.get_subconfig('RASTER_FOUNDRY')
 
@@ -56,86 +104,29 @@ class ObjectDetectionExperiments(rv.ExperimentSet):
             num_steps = 1
             debug = True
 
-        rf_project_id = rf_config('project_id')
-        rf_project_layer_id = rf_config('project_layer_id')
-        refresh_token = rf_config('refresh_token')
-        rf_host = rf_config('rf_api_host')
-        vision_host = rf_config('vision_api_host')
-        ground_truth_annotation_group = rf_config('ground_truth_annotation_group')
+        init = setup(root_uri, rf_config, random_name)
 
-        token = 'Bearer ' + rf.get_api_token(refresh_token, rf_host)
-        vision_project = vision.create_project(token, vision_host, 'object detection example')
-        vision_experiment = vision.create_experiment(
-            token,
-            vision_host,
-            'object detection example',
-            vision_project['id'],
-            'super advanced ml model',
-            'complicated and good',
-            'object detection'
-        )
-        rf_scenes = rf.get_rf_scenes(token, rf_host, rf_project_id, rf_project_layer_id)
+        rs_config = init["raster_source_builder"].with_stats_transformer().build()
+        label_source_config = init["label_source_builder"].build()
 
-        train_store_annotation_group = rf.create_annotation_group(
-            token,
-            rf_host,
-            rf_project_id,
-            rf_project_layer_id,
-            'od-example-train-{}'.format(short_hash)
-        )['id']
-        test_store_annotation_group = rf.create_annotation_group(
-            token,
-            rf_host,
-            rf_project_id,
-            rf_project_layer_id,
-            'od-example-test-{}'.format(short_hash)
-        )['id']
-        validation_store_annotation_group = rf.create_annotation_group(
-            token,
-            rf_host,
-            rf_project_id,
-            rf_project_layer_id,
-            'od-example-validation-{}'.format(short_hash)
-        )['id']
-
-        rs_config = RasterioSourceConfig.builder(rv.RASTERIO_SOURCE) \
-            .with_uris([unquote(x["ingestLocation"]) for x in rf_scenes]) \
-            .build()
-        raster_source = rs_config.create_source(tmp_dir)
-
-        label_source_config = RfLabelSourceConfigBuilder() \
-            .with_annotation_group(ground_truth_annotation_group) \
-            .with_project_id(rf_project_id) \
-            .with_project_layer_id(rf_project_layer_id) \
-            .with_refresh_token(refresh_token) \
-            .with_crs_transformer(raster_source.get_crs_transformer()) \
-            .build()
-        label_source = label_source_config.create_source()
-
-        base_store_builder = RfLabelStoreConfigBuilder() \
-            .with_project_id(rf_project_id) \
-            .with_project_layer_id(rf_project_layer_id) \
-            .with_refresh_token(refresh_token) \
-            .with_crs_transformer(raster_source.get_crs_transformer()) \
-            .with_class_map(label_source._class_map)
+        base_store_builder = ObjectDetectionGeoJSONStoreConfig.builder(rv.OBJECT_DETECTION_GEOJSON)
 
         train_label_store_config = base_store_builder \
-            .with_annotation_group(train_store_annotation_group) \
-            .build()
-        test_label_store_config = base_store_builder \
-            .with_annotation_group(test_store_annotation_group) \
+            .with_uri(os.path.join(root_uri, 'rv-labels', 'train')) \
             .build()
         validation_label_store_config = base_store_builder \
-            .with_annotation_group(validation_store_annotation_group) \
+            .with_uri(os.path.join(root_uri, 'rv-labels', 'validation')) \
+            .build()
+        test_label_store_config = base_store_builder \
+            .with_uri(os.path.join(root_uri, 'rv-labels', 'test')) \
             .build()
 
-        task_config = TaskConfig.builder(rv.OBJECT_DETECTION) \
+        task_config = rv.TaskConfig.builder(rv.OBJECT_DETECTION) \
             .with_chip_size(200) \
-            .with_classes({
-                'fb3bb8b9-478b-4106-b13d-d7410aa2cb60': (1, 'red')
-            }).build()
+            .with_classes(init["task_class_map"]) \
+            .build()
 
-        base_scene_builder = SceneConfig.builder() \
+        base_scene_builder = rv.SceneConfig.builder() \
             .with_task(task_config) \
             .with_raster_source(rs_config)
 
@@ -155,18 +146,15 @@ class ObjectDetectionExperiments(rv.ExperimentSet):
             .with_id('example test scene') \
             .build()
 
-        dataset_config = DatasetConfig.builder() \
+        dataset_config = rv.DatasetConfig.builder() \
             .with_train_scenes([train_scene_config]) \
             .with_validation_scenes([validation_scene_config]) \
             .with_test_scenes([test_scene_config]) \
             .build()
 
-        evaluator = VisionObjectDetectionEvaluatorConfigBuilder() \
-            .with_project_id(vision_project['id']) \
-            .with_experiment_id(vision_experiment['id']) \
-            .with_refresh_token(refresh_token) \
-            .with_class_map(label_source._class_map) \
-            .with_output_uri(tmp_dir) \
+        evaluator = ObjectDetectionEvaluatorConfig.builder(rv.OBJECT_DETECTION_EVALUATOR) \
+            .with_output_uri(os.path.join(root_uri, 'rv-evaluation')) \
+            .with_class_map(init["task_class_map"]) \
             .build()
 
         backend_config = rv.BackendConfig.builder(rv.TF_OBJECT_DETECTION) \
@@ -178,9 +166,9 @@ class ObjectDetectionExperiments(rv.ExperimentSet):
             .build()
 
         # then create an experiment config that uses the evaluator and the task and the dataset
-        experiment_config = ExperimentConfig.builder() \
+        experiment_config = rv.ExperimentConfig.builder() \
             .with_id('testexperiment') \
-            .with_root_uri(tmp_dir) \
+            .with_root_uri(root_uri) \
             .with_task(task_config) \
             .with_backend(backend_config) \
             .with_dataset(dataset_config) \
